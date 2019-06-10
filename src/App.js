5197af268e9df4bf47ab1323da8fcbf51950fd79
Chia Ling <chia.ling@modolabs.com>
2019-06-10 14:15:23 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  Thread,
+  Window
+} from "stream-chat-react";
+import { MessageList, MessageInput } from "stream-chat-react";
+import { StreamChat } from "stream-chat";
 
-import Chat from "./Chat";
-import Login from "./Login";
+import "stream-chat-react/dist/css/index.css";
 
-import UnauthedRoute from "./UnauthedRoute";
-import AuthedRoute from "./AuthedRoute";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.client = new StreamChat('brbgs2r8pfvk');
 
-const App = () => (
-  <Router>
-    <Switch>
-      <UnauthedRoute path="/auth/login" component={Login} />
-      <AuthedRoute path="/" component={Chat} />
-    </Switch>
-  </Router>
-);
+    this.client.setUser(
+      {
+        id: "steve-smith",
+        name: "Steve Smith",
+        image: "https://getstream.io/random_svg/?id=steve-smith&name=Steve+smith"
+      },
+      //localStorage.getItem("token")
+      this.client.devToken('steve-smith'),
+    );
+
+    this.channel = this.client.channel("messaging", "godevs", {
+      image:
+        "https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png",
+      name: "Talk about Anthropology"
+    });
+  }
+
+  render() {
+    return (
+      <Chat client={this.client} theme={"messaging light"}>
+        <Channel channel={this.channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
+    );
+  }
+}
 
 export default App;
