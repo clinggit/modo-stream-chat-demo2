c8543e4f5f719e38a69fc088ea5fc64623c80e5d
Chia Ling <chia.ling@modolabs.com>
2019-06-10 14:06:23 -0700

    Updating files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import React from 'react';
-import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
-import { MessageList, MessageInput } from 'stream-chat-react';
-import { StreamChat } from 'stream-chat';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-import 'stream-chat-react/dist/css/index.css';
+import Chat from "./Chat";
+import Login from "./Login";
 
-const chatClient = new StreamChat('brbgs2r8pfvk');
-//const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidHdpbGlnaHQtbGFiLTIifQ.9CrUddtFnRQjzsh3wlPj-sm3nCxc5VG7h7v6KqI92xI';
-
-chatClient.setUser(
-  {
-       id: 'edbellah',
-       name: 'Ed',
-       image: 'https://getstream.io/random_svg/?id=ed&name=Ed+Bellah'
-  },
-  chatClient.devToken('ed'),
-);
-
-const channel = chatClient.channel('messaging', 'godevs', {
-  // add as many custom fields as you'd like
-  image: 'https://static.modolabs.com/cling/anthropology.png',
-  name: 'Talk about Anthropology',
-});
+import UnauthedRoute from "./UnauthedRoute";
+import AuthedRoute from "./AuthedRoute";
 
 const App = () => (
-  <Chat client={chatClient} theme={'messaging light'}>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
+  <Router>
+    <Switch>
+      <UnauthedRoute path="/auth/login" component={Login} />
+      <AuthedRoute path="/" component={Chat} />
+    </Switch>
+  </Router>
 );
 
 export default App;
