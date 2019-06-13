95f5d5367e333d3ae77eb47ec22f063e372c728f
Chia Ling <chia.ling@modolabs.com>
2019-06-13 10:57:13 -0700

    Added ChannelList

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
-import React, { Component } from "react";
-import {
-  Chat,
-  Channel,
-  ChannelHeader,
-  Thread,
-  Window
-} from "stream-chat-react";
-import { MessageList, MessageInput } from "stream-chat-react";
-import { StreamChat } from "stream-chat";
+import React from 'react';
+import { Chat, Channel, ChannelList, Window } from 'stream-chat-react';
+import { ChannelHeader, MessageList } from 'stream-chat-react';
+import { MessageInput, Thread } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
 
-import "stream-chat-react/dist/css/index.css";
+import 'stream-chat-react/dist/css/index.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.client = new StreamChat('brbgs2r8pfvk');
+const chatClient = new StreamChat('brbgs2r8pfvk');
+//const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY3JpbXNvbi1uaWdodC04In0.hRhOSIgRh3yHLyVPU5N9D3fB1Dr02p1Qeo8HDlPATLA';
 
-    this.client.setUser(
-      {
-        id: "steve-smith",
-        name: "Steve Smith",
-        image: "https://getstream.io/random_svg/?id=steve-smith&name=Steve+smith"
-      },
-      //localStorage.getItem("token")
-      this.client.devToken('steve-smith'),
-    );
+chatClient.setUser(
+  {
+    id: 'ed-bellah',
+    name: 'Ed Bellah',
+    image: 'https://getstream.io/random_svg/?id=ed-bellah&name=Ed+Bellah'
+  },
+  //userToken,
+  chatClient.devToken('ed-bellah'),
+);
 
-    this.channel = this.client.channel("messaging", "godevs", {
-      image:
-        "https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png",
-      name: "Talk about Anthropology"
-    });
-  }
+const filters = { type: 'messaging' };
+const sort = { last_message_at: -1 };
+const channels = chatClient.queryChannels(filters, sort);
 
-  render() {
-    return (
-      <Chat client={this.client} theme={"messaging light"}>
-        <Channel channel={this.channel}>
-          <Window>
-            <ChannelHeader />
-            <MessageList />
-            <MessageInput />
-          </Window>
-          <Thread />
-        </Channel>
-      </Chat>
-    );
-  }
-}
+const App = () => (
+  <Chat client={chatClient} theme={'messaging light'}>
+    <ChannelList
+      filters={filters}
+      sort={sort}
+    />
+    <Channel>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
-export default App;
+export default App; 
